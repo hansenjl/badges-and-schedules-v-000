a1d09ffb62644d450d0e3ebbc7139e4349534131
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-06-01 00:58:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,14 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array_of_names)
-  #badges = []
-  # array_of_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badges = []
+
+  
+  array_of_names.each do |name|
+     badges << badge_maker(name)
+  end
+  
+  badges 
   
   array_of_names.map do |name|
      badge_maker(name)
